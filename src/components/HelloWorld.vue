7b97934a4558cb2f5ec042af3cfe68ba74c76e5a
sandy saputra <ss5679848@gmail.com>
2023-07-01 15:14:51 +0700

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,114 +1,90 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+  <div>
+    <h2>Tampilan Klasemen</h2>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>No</th>
+          <th>Klub</th>
+          <th>Ma</th>
+          <th>Me</th>
+          <th>S</th>
+          <th>K</th>
+          <th>GM</th>
+          <th>GK</th>
+          <th>Point</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(team, index) in teams" :key="index">
+          <td>{{ index + 1 }}</td>
+          <td>{{ team.club }}</td>
+          <td>{{ team.ma }}</td>
+          <td>{{ team.me }}</td>
+          <td>{{ team.s }}</td>
+          <td>{{ team.k }}</td>
+          <td>{{ team.gm }}</td>
+          <td>{{ team.gk }}</td>
+          <td>{{ team.point }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
 export default {
-  name: "HelloWorld",
-  props: {
-    msg: String,
+  data() {
+    return {
+      teams: [
+        {
+          club: "Persib",
+          ma: 2,
+          me: 2,
+          s: 0,
+          k: 0,
+          gm: 4,
+          gk: 0,
+          point: 6,
+        },
+        {
+          club: "Arema",
+          ma: 2,
+          me: 1,
+          s: 0,
+          k: 1,
+          gm: 2,
+          gk: 2,
+          point: 3,
+        },
+        {
+          club: "Persija",
+          ma: 2,
+          me: 0,
+          s: 0,
+          k: 2,
+          gm: 0,
+          gk: 4,
+          point: 0,
+        },
+        {
+          club: "psis",
+          ma: 2,
+          me: 0,
+          s: 0,
+          k: 2,
+          gm: 0,
+          gk: 4,
+          point: 0,
+        },
+      ],
+    };
   },
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style>
+.table {
+  margin: auto;
+  width: 50% !important;
 }
 </style>
